7e2cec8792781bfc6e6cc274af986707d04a28a4
Qoda <39283833+qodatecnologia@users.noreply.github.com>
2019-08-07 14:25:37 -0300

    Add files via upload

---
+++ webhook-cadastro-firebase.js
@@ -0,0 +1,16 @@
+const admin = require('firebase-admin');
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'SUA_URL_FIREBASE',
+});
+
+function cadastro(agent) {
+    var newKey = admin.database().ref().child('agendados').push().key; //cria chave única
+    
+    admin.database().ref('agendados/'+newKey).set({ //insere infos no database
+        servico: agent.parameters['servico'],
+		date: agent.parameters['date'],
+		time: agent.parameters['time']
+    });
+
+    agent.add('Confirmado!'); //confirma agendamento
