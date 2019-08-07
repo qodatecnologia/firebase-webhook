5546576da3c51490d7ae797833cb39fcbd539e8d
Qoda <39283833+qodatecnologia@users.noreply.github.com>
2019-08-07 15:33:47 -0300

    Update webhook-cadastro-firebase.js

--- webhook-cadastro-firebase.js
+++ webhook-cadastro-firebase.js
@@ -4,13 +4,11 @@ admin.initializeApp({
   databaseURL: 'SUA_URL_FIREBASE',
 });
 
-function cadastro(agent) {
+function cadastro_func(agent) {
     var newKey = admin.database().ref().child('agendados').push().key; //cria chave única
     
     admin.database().ref('agendados/'+newKey).set({ //insere infos no database
-        servico: agent.parameters['servico'],
-		date: agent.parameters['date'],
-		time: agent.parameters['time']
+        servico: agent.parameters.servico,
+		date: agent.parameters.date,
+		time: agent.parameters.time
     });
-
-    agent.add('Confirmado!'); //confirma agendamento
